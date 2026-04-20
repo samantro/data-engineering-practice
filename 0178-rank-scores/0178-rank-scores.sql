d74b598cdcc2cb0869c3a4c15079e0f14b6f12f8
samantro <66985251+samantro@users.noreply.github.com>
2026-04-20 08:04:28 +0530

    Time: 287 ms (80.67%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,15 +1,4 @@
-# Write your MySQL query statement below
-with cte as (
-    select distinct score 
-    from scores
-), 
-cte2 as (
-    select score, 
-    rank() over (order by score desc) as 'rank'
-    from cte
-)
-
-select s.score, c.rank 
-from cte2 c
-left join scores s
-on s.score = c.score;
+SELECT score,
+       DENSE_RANK() OVER (ORDER BY score DESC) AS `rank`
+FROM Scores
+ORDER BY score DESC;
