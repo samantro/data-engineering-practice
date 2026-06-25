2b536f3350a50e50d771edf8483d70da40b77162
samantro <66985251+samantro@users.noreply.github.com>
2026-06-25 16:00:08 +0530

    Time: 661 ms (80.88%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,11 +1,14 @@
-# Write your MySQL query statement below
-with cte as (
-    select max(salary) as s, id, departmentId
-    from employee
-    group by departmentId
-)
-
-select d.name as Department,e.name as Employee, s as salary
-from cte c
-left join Employee e on c.departmentId = e.departmentId and  c.s = e.salary
-left join Department d on c.departmentId = d.id;
+SELECT Department, Employee, Salary
+FROM (
+    SELECT d.name AS Department,
+           e.name AS Employee,
+           e.salary AS Salary,
+           DENSE_RANK() OVER (
+               PARTITION BY e.departmentId
+               ORDER BY e.salary DESC
+           ) AS rnk
+    FROM Employee e
+    JOIN Department d
+      ON e.departmentId = d.id
+) t
+WHERE rnk = 1;
